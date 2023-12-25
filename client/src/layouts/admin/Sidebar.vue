cd824bfbfe4c22cb51a58f3645044ffbd6b37242
HeeJae RA <rhj7369@nate.com>
2023-12-25 21:39:00 +0900

    테이블

--- client/src/layouts/admin/Sidebar.vue
+++ client/src/layouts/admin/Sidebar.vue
@@ -6,7 +6,7 @@
 				<div class="sb-sidenav-menu">
 					<div class="nav">
 						<div class="sb-sidenav-menu-heading">Core</div>
-						<a class="nav-link" href="index.html">
+						<a class="nav-link" href="#">
 							<div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
 							Dashboard
 						</a>
@@ -30,8 +30,8 @@
 							data-bs-parent="#sidenavAccordion"
 						>
 							<nav class="sb-sidenav-menu-nested nav">
-								<a class="nav-link" href="layout-static.html">Static Navigation</a>
-								<a class="nav-link" href="layout-sidenav-light.html">Light Sidenav</a>
+								<a class="nav-link" href="#">Static Navigation</a>
+								<a class="nav-link" href="#">Light Sidenav</a>
 							</nav>
 						</div>
 						<a
@@ -71,9 +71,9 @@
 									data-bs-parent="#sidenavAccordionPages"
 								>
 									<nav class="sb-sidenav-menu-nested nav">
-										<a class="nav-link" href="login.html">Login</a>
-										<a class="nav-link" href="register.html">Register</a>
-										<a class="nav-link" href="password.html">Forgot Password</a>
+										<a class="nav-link" href="#">Login</a>
+										<a class="nav-link" href="#">Register</a>
+										<a class="nav-link" href="#">Forgot Password</a>
 									</nav>
 								</div>
 								<a
@@ -94,19 +94,19 @@
 									data-bs-parent="#sidenavAccordionPages"
 								>
 									<nav class="sb-sidenav-menu-nested nav">
-										<a class="nav-link" href="401.html">401 Page</a>
-										<a class="nav-link" href="404.html">404 Page</a>
-										<a class="nav-link" href="500.html">500 Page</a>
+										<a class="nav-link" href="#">401 Page</a>
+										<a class="nav-link" href="#">404 Page</a>
+										<a class="nav-link" href="#">500 Page</a>
 									</nav>
 								</div>
 							</nav>
 						</div>
 						<div class="sb-sidenav-menu-heading">Addons</div>
-						<a class="nav-link" href="charts.html">
+						<a class="nav-link" href="#">
 							<div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
 							Charts
 						</a>
-						<a class="nav-link" href="tables.html">
+						<a class="nav-link" href="#">
 							<div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
 							Tables
 						</a>
