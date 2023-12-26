c82812afd11903f2770631fa647b1365ceae855a
starhumang <starhumang@naver.com>
2023-12-26 15:47:32 +0900

    2023.12-26오후

--- client/src/views/user/TestLogin.vue
+++ client/src/views/user/TestLogin.vue
@@ -5,14 +5,19 @@
             <input type="text" id="userId" required="아이디를 입력해주세요" v-model="userInfo.userId">
             비밀번호
             <input type="password" id="userPw" required="비밀번호를 입력해주세요" v-model="userInfo.userPw">
-            <button class="btn" @click="login()">로그인하기</button>
+            <button @click="login()">로그인하기</button>
+            <button @click="login()">회원가입</button>
         </div>
 
     </div>
 </template>
 
 <script>
+/*1. npm install --save sweetalert2 설치
+  2. import Swal from "sweetalert2 시킴
+  3. 사용하면 됨*/
 import axios from 'axios';
+import Swal from "sweetalert2"
 export default {
     data() {
         return {
@@ -31,11 +36,31 @@ export default {
                 }
             }
             let result = await axios.post("/node/login", obj).catch(err => console.log(err));
-            console.log("result : ", result)
-            if(result.check == "다맞음"){
-                //라우터로 홈으로 보내기
+            console.log("result.check : ", result.data.check);
+            
+             //로그인 상태별 alert창
+            if(result.data.check == "아이디틀림"){
+                Swal.fire({
+                icon: "warning",
+                title: "로그인 실패.",
+                text: "존재하지 않는 아이디입니다.",
+            });
+            }else if(result.data.check == "비번틀림"){
+                Swal.fire({
+                icon: "warning",
+                title: "로그인 실패.",
+                text: "존재하지 않는 비밀번호입니다.",
+            });
+            }else if(result.data.check == "다맞음"){
+                Swal.fire({
+                icon: "success",
+                title: "로그인 성공.",
+                text: "즐거운 이용되시기 바랍니다.",
+            })
             }
-        }
+        },
+        
+       
     }
 }
 </script>
--- server/app.js
+++ server/app.js
@@ -9,9 +9,11 @@ app.listen(3000, () => {
 	console.log('서버 시작');
 });
 
+//로그인ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+
 app.post("/login", async(request, response)=>{
 	let data = request.body.param;
-	console.log("data : ", data);
+	console.log("data : ", data.userId);
 	
 	let result = await mysql.query("login", data.userId);
 	console.log("result : ", result);
@@ -19,21 +21,26 @@ app.post("/login", async(request, response)=>{
 	let reps = {
 		check : "",
 	}
-	if(result){
-		if(result.userPw == data.userPw){
-			reps.check = "다맞음"
-			reps.userName = result.user_name
+	if(result.length !=0){
+		// console.log("result.length = ",result.length);
+		// console.log("data.userPw  = ",data.userPw);
+		// console.log("result.user_pw  = ",result[0].user_pw);//비밀번호
+		
+		if(result[0].user_pw == data.userPw){
+			reps.check = "다맞음";
+			console.log("result.user_id  = ",result[0].user_id);
 
 			//세션추가
-			
+			window.localStorage.setItem('userId', result[0].user_id); //키 값 : userId, 데이터 : user1
+			const userId = window.localStorage.getItem('userId');
+			console.log(userId);
+
 		}else{
 			reps.check = "비번틀림"
-			
 		}
 	}else{
 		reps.check = "아이디틀림"
 	}
 	response.send(reps);
-	
-	
+	console.log("reps.check : ", reps.check);
 })
