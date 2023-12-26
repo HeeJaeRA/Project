a1774d80d61ab1df7cc0a434a786cc429a81dd89
starhumang <starhumang@naver.com>
2023-12-26 17:50:27 +0900

    2023-12-26 학원끝

--- client/src/views/user/TestLogin.vue
+++ client/src/views/user/TestLogin.vue
@@ -5,8 +5,8 @@
             <input type="text" id="userId" required="아이디를 입력해주세요" v-model="userInfo.userId">
             비밀번호
             <input type="password" id="userPw" required="비밀번호를 입력해주세요" v-model="userInfo.userPw">
-            <button @click="login()">로그인하기</button>
-            <button @click="login()">회원가입</button>
+            <button class="btn" @click="login()">로그인하기</button>
+            <button class="btn" @click="login()">회원가입</button>
         </div>
 
     </div>
@@ -36,8 +36,8 @@ export default {
                 }
             }
             let result = await axios.post("/node/login", obj).catch(err => console.log(err));
-            console.log("result.check : ", result.data.check);
-            
+            console.log("result : ", result.data);
+
              //로그인 상태별 alert창
             if(result.data.check == "아이디틀림"){
                 Swal.fire({
@@ -52,12 +52,21 @@ export default {
                 text: "존재하지 않는 비밀번호입니다.",
             });
             }else if(result.data.check == "다맞음"){
-                Swal.fire({
+                await Swal.fire({
                 icon: "success",
                 title: "로그인 성공.",
                 text: "즐거운 이용되시기 바랍니다.",
             })
+            
+            //세션추가
+            window.localStorage.removeItem('userId');
+			window.localStorage.setItem('userId', result.data.id); //키 값 : userId, 데이터 : user1
+			const userId = window.localStorage.getItem('userId');
+			console.log("userId = ",userId);
             }
+            
+            await this.$router.push('/home');
+            this.$router.go(0);
         },
         
        
--- server/app.js
+++ server/app.js
@@ -20,6 +20,7 @@ app.post("/login", async(request, response)=>{
 
 	let reps = {
 		check : "",
+		id : ""
 	}
 	if(result.length !=0){
 		// console.log("result.length = ",result.length);
@@ -28,12 +29,8 @@ app.post("/login", async(request, response)=>{
 		
 		if(result[0].user_pw == data.userPw){
 			reps.check = "다맞음";
+			reps.id = result[0].user_id;
 			console.log("result.user_id  = ",result[0].user_id);
-
-			//세션추가
-			window.localStorage.setItem('userId', result[0].user_id); //키 값 : userId, 데이터 : user1
-			const userId = window.localStorage.getItem('userId');
-			console.log(userId);
 
 		}else{
 			reps.check = "비번틀림"
