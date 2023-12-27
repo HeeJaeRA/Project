d2c6cd4bcea2ae6d4fe88cc743f0f99c6cfa61ab
starhumang <starhumang@naver.com>
2023-12-27 09:10:25 +0900

    d

--- server/app.js
+++ server/app.js
@@ -9,31 +9,36 @@ app.listen(3000, () => {
 	console.log('서버 시작');
 });
 
+//로그인ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+
 app.post("/login", async(request, response)=>{
 	let data = request.body.param;
-	console.log("data : ", data);
+	console.log("data : ", data.userId);
 	
 	let result = await mysql.query("login", data.userId);
 	console.log("result : ", result);
 
 	let reps = {
 		check : "",
+		id : ""
 	}
-	if(result){
-		if(result.userPw == data.userPw){
-			reps.check = "다맞음"
-			reps.userName = result.user_name
+	if(result.length !=0){
+		// console.log("result.length = ",result.length);
+		// console.log("data.userPw  = ",data.userPw);
+		// console.log("result.user_pw  = ",result[0].user_pw);//비밀번호
+		
+		if(result[0].user_pw == data.userPw){
+			reps.check = "다맞음";
+			reps.id = result[0].user_id;
+			console.log("result.user_id  = ",result[0].user_id);
 
-			//세션추가
-			
 		}else{
 			reps.check = "비번틀림"
-			
 		}
 	}else{
 		reps.check = "아이디틀림"
 	}
 	response.send(reps);
-	
-	
+	console.log("reps.check : ", reps.check);
 })
+
