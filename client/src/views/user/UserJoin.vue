a99e5d39440243474cdd293b337c58d8f5e84a45
HeeJae RA <rhj7369@nate.com>
2023-12-29 04:29:10 +0900

    merge

--- client/src/views/user/UserJoin.vue
+++ client/src/views/user/UserJoin.vue
@@ -1,235 +1,129 @@
 <template>
-    <div id="container">
-		<h1>회원 가입</h1>
-		<hr>
-		<form style="width:700px; height:800px; text-align:center">
-			<ul>
-				<li>
-					<label class="field">▶ 아이디</label>
-					<input type="text" user_id="id" placeholder="4~15자리의 영문과 숫자로 입력" required v-model="userInfo.user_id">
-                    <button class="btn btn-success rounded-pill px-3" @click="checkDuplication()" type="button">중복확인</button>
-				</li>
-				<li>
-					<label class="field">▶ 비밀번호</label>
-					<input type="password" id="password" name="user-pw1" placeholder="8자리 이상" required v-model="userInfo.user_pw">
-				</li>
-				<li>
-					<label class="field">▶ 비밀번호 재확인</label>
-					<input type="text" placeholder="4~15자리의 영문과 숫자로 입력" required>
-				</li>
-                
-            </ul>
-            <br><br><hr>
-            
-            <ul id="user-info">
-                <li>
-					<label for="user-pw2" class="field">▶ 이름</label>
-					<input type="text" id="name" placeholder="홍길동" required v-model="userInfo.user_name">
-				</li>
-                <li>
-					<label for="user-pw2" class="field">▶ 닉네임</label>
-					<input type="text" id="nickname" placeholder="길동이" required v-model="userInfo.nickname">
-                    <button class="btn btn-success rounded-pill px-3" type="button">중복확인</button>
-				</li>
-                <li>
-					<label for="user-pw2" class="field">▶ 생년월일</label>
-					<input type="text" id="birth" placeholder="1993-01-01" required v-model="userInfo.birthday">
-				</li>
-                <li>
-					<label for="user-pw2" class="field">▶ 전화번호</label>
-					<input type="text" id="tel" placeholder="010-1111-1111" required v-model="userInfo.phone">
-                    <button class="btn btn-success rounded-pill px-3" type="button">본인인증</button>
-				</li>
-                <li>
-					<label for="user-pw2" id="picture" class="field">▶ 프로필사진</label>
-					<input type="file" style="width=350px; height=35px" >
-                    
-				</li>
-                <li>
-                    <label for="user-pw2" id="gender" class="field">▶ 성별</label>
-					<label style="margin-right:80px;">
-                        <input type="radio" name ="gender" value="M" v-model="userInfo.gender">남성 
-                    </label>
+	<div class="container">
+		<main class="form-signin w-100 m-auto">
+			<form style="width: 400px; height: 600px; margin: 0 auto">
+				<br /><br /><br />
 
-                    <label class="radio">
-                        <input type="radio"  name ="gender" value="F" v-model="userInfo.gender">여성
-                    </label>
-				</li>
-                <li>
-                    <div>
-                    <input type="checkbox">
-                    개인정보 이용 및 수집에 동의합니다.
-                    </div>
-                </li>             
-			</ul>
-            <button class="btn btn-primary w-100 py-2" @click="userInsert()" type="button">제출하기</button>
+				<div style="text-align: center">
+					<h1 class="h3 mb-3 fw-normal">로그인</h1>
 
-		</form>
+					<div class="form-floating">
+						<input
+							type="text"
+							class="form-control"
+							id="floatingInput"
+							placeholder="user1"
+							v-model="userInfo.userId"
+						/>
+						<label for="floatingInput">Id</label>
+					</div>
+					<br />
+
+					<div class="form-floating">
+						<input
+							type="password"
+							class="form-control"
+							id="floatingPassword"
+							placeholder="Password"
+							v-model="userInfo.userPw"
+						/>
+						<label for="floatingPassword">Password</label>
+					</div>
+
+					<div>
+						<br />
+						<button class="btn btn-primary w-100 py-2" @click="login()" type="button">로그인하기</button>
+					</div>
+
+					<div style="text-align: right">
+						<div>
+							<br />
+							빠른 가입을 원하시나요?
+							<button class="btn btn-warning rounded-pill px-3" type="button">카카오로 로그인하기</button>
+						</div>
+
+						<div>
+							<br />
+							대다내의 회원이 되어보세요!
+							<button
+								class="btn btn-success rounded-pill px-3"
+								@click="$router.push('/join')"
+								type="button"
+							>
+								회원가입
+							</button>
+						</div>
+					</div>
+					<div>
+						<br /><br />
+						<a style="font-size: 17px; text-decoration-line: none" href="">아이디찾기 </a>/
+						<a style="font-size: 17px; text-decoration-line: none" href=""> 비밀번호 찾기</a>
+					</div>
+				</div>
+			</form>
+		</main>
 	</div>
 </template>
 
 <script>
+/*1. npm install --save sweetalert2 설치
+  2. import Swal from "sweetalert2 시킴
+  3. 사용하면 됨*/
 import axios from 'axios';
-import Swal from "sweetalert2"
+import Swal from 'sweetalert2';
 export default {
-    data(){
-        return{
-            userInfo : {
+	data() {
+		return {
+			userInfo: {
+				userId: '',
+				userPw: '',
+			},
+		};
+	},
+	methods: {
+		async login() {
+			let obj = {
+				param: {
+					userId: this.userInfo.userId,
+					userPw: this.userInfo.userPw,
+				},
+			};
+			let result = await axios.post('/node/login', obj).catch((err) => console.log(err));
+			console.log('loginresult : ', result);
 
-                user_id : '',
-                user_pw : '',
-                user_name : '',
-                nickname : '',
-                phone : '',
-                profile : '',
-                gender : '',
-                birthday : '',               
-                user_status : '',
-                grade : '',
-                sns_status : '',
-            }
-        }
-    },
-    created(){
-        this.userInfo.user_status ='일반유저',
-        this.userInfo.grade ='맛초보',
-        this.userInfo.sns_status='사이트계정'
-    },
-    methods:{
-        async userInsert(){
-            let obj ={
-                param:{
-                    user_id : this.userInfo.user_id,
-                    user_pw : this.userInfo.user_pw,
-                    user_name : this.userInfo.user_name,
-                    nickname : this.userInfo.nickname,
-                    phone : this.userInfo.phone,
-                    profile : this.userInfo.profile,
-                    gender : this.userInfo.gender,
-                    birthday : this.userInfo.birthday,
-                    user_status : this.userInfo.user_status,
-                    grade : this.userInfo.grade,
-                    sns_status : this.userInfo.sns_status
-                }
-            }
-            let result = await axios.post("/node/join", obj).catch(err => console.log(err));
-            console.log("joinresult : ", result);
-            if(result.data.affectedRows > 0){
-                Swal.fire({
-                icon: "success",
-                title: "회원가입 성공",
-                html: "대다내 회원가입을 축하합니다!<br/>로그인 후 이용해주세요",
-            });
-            await this.$router.push('/login');
+			//로그인 상태별 alert창
+			if (result.data.check == '아이디틀림') {
+				Swal.fire({
+					icon: 'warning',
+					title: '로그인 실패.',
+					text: '존재하지 않는 아이디입니다.',
+				});
+			} else if (result.data.check == '비번틀림') {
+				Swal.fire({
+					icon: 'warning',
+					title: '로그인 실패.',
+					text: '비밀번호가 틀렸습니다.',
+				});
+			} else if (result.data.check == '다맞음') {
+				await Swal.fire({
+					icon: 'success',
+					title: '로그인 성공.',
+					text: '즐거운 이용되시기 바랍니다.',
+				});
 
-            }else{
-                 Swal.fire({
-                icon: "warning",
-                title: "회원가입 실패",
-                text: "회원가입에 실패하였습니다. 코드를 고쳐주세요",
-            });
-            }
-        },
-        async checkDuplication(){
-            let obj ={
-                param :{
-                    user_id : this.userInfo.user_id
-                }
-            }
-            let result = await axios.post("/node/join", obj).catch(err => console.log(err));
-            console.log("vue중복체크result = ", result.data );
-        }
+				//브라우저 세션추가
+				window.localStorage.removeItem('userId');
+				window.localStorage.setItem('userId', result.data.id); //키 값 : userId, 데이터 : user1
+				const userId = window.localStorage.getItem('userId');
+				console.log('userId = ', userId);
 
-
-
-
-    
-        // idCheck(){
-        //     let idLeng = id.value.length;
-        //     if(idLeng < 4 || idLeng > 15){
-        //         alert(`4글자 이상, 15글자 이하로 아이디를 입력해주세요`);
-        //         id.select();
-        //     }
-        // },
-
-        // pw1Check(){
-        //     let pw1Leng = pw1.value.length;
-        //     if(pw1Leng < 8){
-        //         alert(`8자리 이상의 비밀번호를 입력해주세요`);
-        //         pw1.value="";
-        //         pw1.focus();
-        //     }
-        // },
-
-        // pw2Compare(){
-        //     if(pw2 !== pw1){
-        //         alert(`비밀번호가 일치하지 않습니다`);
-        //         pw2.value= '';
-        //         pw2.focus();
-        //     }else{
-   
-        //     }
-        // }
-    }
-}
+				//로그인 성공 후 홈으로 이동
+				await this.$router.push('/home');
+				this.$router.go(0);
+			}
+		},
+	},
+};
 </script>
 
-
-<style scoped>
-#container{
-	width:700px;
-	margin:0 auto;
-    
-}
-h1{
-	text-align: center;
-}
-ul li {
-	list-style:none;
-	clear:both;
-    padding-bottom: 20px;
-    padding-top: 10px;
-}
-
-.field {
-	float:left;
-	width:150px;
-	font-weight:bold;
-	font-size:0.9em;
-	line-height: 55px;
-	text-align:center;
-	margin-right:15px;
-}
-input[type="text"], 
-input[type="password"], 
-input[type="tel"] {
-	float:left;
-	width:350px;
-	height:35px;
-	border:1px solid #aaa;
-	border-radius:5px;
-	padding:5px;
-	margin:10px 0;
-	
-}
-
- .radio {
-	line-height:55px;
-} 
-#buttons{
-	text-align: center;
-}
-/* button {
-	width:200px;
-	height:50px;
-	margin-right:10px;
-	border:1px solid #ccc;
-	border-radius: 20px;
-	background:#f2ffe4;
-	font-size:0.9em;
-    
-} */
-
-</style>
-
-
+<style scoped></style>
--- client/src/views/user/UserLogin.vue
+++ client/src/views/user/UserLogin.vue
@@ -1,51 +1,68 @@
 <template>
-    <div class="container">
-        <main class="form-signin w-100 m-auto">
-        <form style=" width:400px; height:600px; margin:0 auto; ">
-            <br><br><br>
+	<div class="container">
+		<main class="form-signin w-100 m-auto">
+			<form style="width: 400px; height: 600px; margin: 0 auto">
+				<br /><br /><br />
 
-            <div style="text-align:center;">
-                <h1 class="h3 mb-3 fw-normal">로그인</h1>
+				<div style="text-align: center">
+					<h1 class="h3 mb-3 fw-normal">로그인</h1>
 
-                <div class="form-floating">
-                    <input type="text" class="form-control" id="floatingInput" placeholder="user1" v-model="userInfo.userId">
-                    <label for="floatingInput">Id</label>
-                </div>
-                <br>
-                
-                <div class="form-floating">
-                    <input type="password" class="form-control" id="floatingPassword" placeholder="Password" v-model="userInfo.userPw">
-                    <label for="floatingPassword">Password</label>
-                </div>
+					<div class="form-floating">
+						<input
+							type="text"
+							class="form-control"
+							id="floatingInput"
+							placeholder="user1"
+							v-model="userInfo.userId"
+						/>
+						<label for="floatingInput">Id</label>
+					</div>
+					<br />
 
-                <div>
-                    <br>  
-                    <button class="btn btn-primary w-100 py-2" @click="login()" type="button">로그인하기</button>
-                </div>
-            
-            
-                <div style="text-align:right;">
-                    <div>
-                        <br>
-                        빠른 가입을 원하시나요?  
-                        <button class="btn btn-warning rounded-pill px-3" type="button">카카오로 로그인하기</button>
-                    </div>
+					<div class="form-floating">
+						<input
+							type="password"
+							class="form-control"
+							id="floatingPassword"
+							placeholder="Password"
+							v-model="userInfo.userPw"
+						/>
+						<label for="floatingPassword">Password</label>
+					</div>
 
-                    <div>
-                        <br>
-                        대다내의 회원이 되어보세요!   
-                        <button class="btn btn-success rounded-pill px-3"  @click="$router.push('/join')" type="button">회원가입</button>
-                    </div>
-                </div>
-                <div>
-                        <br><br>
-                        <a style="font-size:17px;text-decoration-line:none;" href="">아이디찾기 </a>/
-                        <a style="font-size:17px;text-decoration-line:none;" href=""> 비밀번호 찾기</a>
-                </div>
-            </div>
-        </form>
-        </main>
-    </div>
+					<div>
+						<br />
+						<button class="btn btn-primary w-100 py-2" @click="login()" type="button">로그인하기</button>
+					</div>
+
+					<div style="text-align: right">
+						<div>
+							<br />
+							빠른 가입을 원하시나요?
+							<button class="btn btn-warning rounded-pill px-3" type="button">카카오로 로그인하기</button>
+						</div>
+
+						<div>
+							<br />
+							대다내의 회원이 되어보세요!
+							<button
+								class="btn btn-success rounded-pill px-3"
+								@click="$router.push('/join')"
+								type="button"
+							>
+								회원가입
+							</button>
+						</div>
+					</div>
+					<div>
+						<br /><br />
+						<a style="font-size: 17px; text-decoration-line: none" href="">아이디찾기 </a>/
+						<a style="font-size: 17px; text-decoration-line: none" href=""> 비밀번호 찾기</a>
+					</div>
+				</div>
+			</form>
+		</main>
+	</div>
 </template>
 
 <script>
@@ -53,67 +70,60 @@
   2. import Swal from "sweetalert2 시킴
   3. 사용하면 됨*/
 import axios from 'axios';
-import Swal from "sweetalert2"
+import Swal from 'sweetalert2';
 export default {
-    data() {
-        return {
-            userInfo :{
-                userId : '',
-                userPw : ''
-            }
-        }
-    },
-    methods: {
-        async login(){
-            let obj ={
-                param :{
-                    userId : this.userInfo.userId,
-                    userPw : this.userInfo.userPw
-                }
-            }
-            let result = await axios.post("/node/login", obj).catch(err => console.log(err));
-            console.log("loginresult : ", result);
+	data() {
+		return {
+			userInfo: {
+				userId: '',
+				userPw: '',
+			},
+		};
+	},
+	methods: {
+		async login() {
+			let obj = {
+				param: {
+					userId: this.userInfo.userId,
+					userPw: this.userInfo.userPw,
+				},
+			};
+			let result = await axios.post('/node/login', obj).catch((err) => console.log(err));
+			console.log('loginresult : ', result);
 
-             //로그인 상태별 alert창
-            if(result.data.check == "아이디틀림"){
-                Swal.fire({
-                icon: "warning",
-                title: "로그인 실패.",
-                text: "존재하지 않는 아이디입니다.",
-            });
-            }else if(result.data.check == "비번틀림"){
-                Swal.fire({
-                icon: "warning",
-                title: "로그인 실패.",
-                text: "존재하지 않는 비밀번호입니다.",
-            });
-            }else if(result.data.check == "다맞음"){
-                await Swal.fire({
-                icon: "success",
-                title: "로그인 성공.",
-                text: "즐거운 이용되시기 바랍니다.",
-                
-                })
-                
-                //브라우저 세션추가
-                window.localStorage.removeItem('userId');
-                window.localStorage.setItem('userId', result.data.id); //키 값 : userId, 데이터 : user1
-                const userId = window.localStorage.getItem('userId');
-                console.log("userId = ",userId);
+			//로그인 상태별 alert창
+			if (result.data.check == '아이디틀림') {
+				Swal.fire({
+					icon: 'warning',
+					title: '로그인 실패.',
+					text: '존재하지 않는 아이디입니다.',
+				});
+			} else if (result.data.check == '비번틀림') {
+				Swal.fire({
+					icon: 'warning',
+					title: '로그인 실패.',
+					text: '비밀번호가 틀렸습니다.',
+				});
+			} else if (result.data.check == '다맞음') {
+				await Swal.fire({
+					icon: 'success',
+					title: '로그인 성공.',
+					text: '즐거운 이용되시기 바랍니다.',
+				});
 
-                //로그인 성공 후 홈으로 이동
-                await this.$router.push('/home');
-                this.$router.go(0);
-            }  
-        },
-        
-       
-    }
-}
+				//브라우저 세션추가
+				window.localStorage.removeItem('userId');
+				window.localStorage.setItem('userId', result.data.id); //키 값 : userId, 데이터 : user1
+				const userId = window.localStorage.getItem('userId');
+				console.log('userId = ', userId);
+
+				//로그인 성공 후 홈으로 이동
+				await this.$router.push('/home');
+				this.$router.go(0);
+			}
+		},
+	},
+};
 </script>
 
-
-
-<style scoped>
-
-</style>
+<style scoped></style>
