556fb5f2a5ea1a1c6e9669ba362f85193f1e2714
HeeJae RA <rhj7369@nate.com>
2024-01-02 22:12:32 +0900

    관리자

--- client/src/main.js
+++ client/src/main.js
@@ -22,6 +22,10 @@ import BootstrapVue3 from 'bootstrap-vue-3';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
 
+//관리자
+import '../public/js/admin/scripts.js';
+import '../public/css/styles.css';
+
 createApp(App)
 	.use(router)
 	.use(store)
