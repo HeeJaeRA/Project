0aee71dad27ab0e43e5387ed0e23fa771d43bf82
HeeJae RA <rhj7369@nate.com>
2024-01-05 00:25:46 +0900

    .

--- server/db/sql.js
+++ server/db/sql.js
@@ -126,18 +126,73 @@ module.exports = {
 	rscatelist: `select * from restaurant where category = ?`,
 	// rsallplist: `SELECT * FROM restaurant LIMIT = ?, OFFSET = ?`,
 
-	//관리자
-	// eventList: `SELECT *FROM event`, //관리자- 이벤트 리스트 출력
-	// eventInfo: `select * from event e join coupon c on(e.coupon_code=c.coupon_code) where event_code = ?`, //이벤트단건조회
-	// insertEvent: `INSERT INTO event set ?`, //관리자 - 이벤트 등록
-	// eventUpdate: `UPDATE event SET ? WHERE event_code = ?`, //관리자 - 이벤트 수정
-	// eventDelete: `DELETE FROM event where event_code =?`,
-	// couponList: `select *from coupon`, //쿠폰 전체목록
-	// insertCoupon: `INSERT INTO coupon set?`, //관리자 - 쿠폰등록
-	// insertUserCoupon: `insert into user_coupon set?`, //쿠폰 일괄발급
-	// couponUpdate: `UPDATE coupon set ?  where coupon_code = ?`, //관리자 - 쿠폰수정
-	// gradeUserList: `SELECT *FROM user WHERE grade =? and user_status= '활동회원' `, //관리자 - 등급별회원리스트출력
-	// userList: `SELECT *FROM user where user_status='활동회원'`, //활동회원전체리스트
+	//관리자------------------------------------------------------------------
+	//이벤트
+	eventList: `SELECT *FROM event`, //관리자- 이벤트 리스트 출력
+	eventInfo: `select * from event e join coupon c on(e.coupon_code=c.coupon_code) where event_code = ?`, //이벤트단건조회
+	insertEvent: `INSERT INTO event set ?`, //관리자 - 이벤트 등록
+	eventUpdate: `UPDATE event SET ? WHERE event_code = ?`, //관리자 - 이벤트 수정
+	eventDelete: `DELETE FROM event where event_code =?`, //이벤트삭제
+	// 쿠폰
+	couponList: `select *from coupon`, //쿠폰 전체목록
+	couponInfo: `select *from coupon where coupon_code=?`, //쿠폰 단건조회
+	insertCoupon: `INSERT INTO coupon set?`, //관리자 - 쿠폰등록
+	couponDelete: `delete from coupon where coupon_code=?`, //쿠폰삭제
+	insertUserCoupon: `insert ignore into  user_coupon set?`, //쿠폰 일괄발급
+	couponUpdate: `UPDATE coupon set ?  where coupon_code = ?`, //관리자 - 쿠폰수정
+	//일반회원
+	gradeUserList: `SELECT *FROM user WHERE grade =? and user_status= '활동회원' `, //관리자 - 등급별회원리스트출력
+	adminuserList: `SELECT *FROM user where user_status='활동회원'`, //활동회원전체리스트
+	allUserList: `select *from user`, //전체 일반회원리스트
+	adminGetUserInfo: `select *from user where user_id=?`, //회원한건조회
+	adminGetpenalty: `select penalty from user where user_id=?`, //회원패널티조회
+
+	//업체
+	adminConfirmSeller: `select *from restaurant where rs_status ='승인대기'`, //승인대기목록업체
+	adminApprove: `update restaurant set rs_status =? where rs_code=?;`, //영업승인/반려하기
+	adminRsList: `select *from restaurant where rs_status ='영업승인'`, //영업승인된 업체리스트
+
+	//qna
+	adminSellerQna: `select *from qna where user_divison = ? and ans_code=0`, //전체목록
+	adminSellerQnaCategory: `select *from qna where ans_code=0 and user_divison = ? and qna_divison = ?`, //전체목록-카테고리
+	adminSellerNqna: `select *from qna where qna_status='답변대기' and user_divison=? and ans_code=0`, // 답변대기
+	adminSellerWaitCategory: `select *from qna where ans_code=0 and user_divison = ?and qna_status ='답변대기' and qna_divison = ?`, //답변대기-카테고리
+	adminSellerQnaDone: `select *from qna where qna_status='답변완료' and user_divison=? and ans_code=0`, //답변완료
+	adminSellerQnaDoneCategory: `select *from qna where qna_status='답변완료' and user_divison=? and ans_code=0 and qna_divison = ?`, //답변완료 -카테고리
+
+	//관리자 답변
+	adminQnaInsert: `insert into qna set ?`,
+	//답변달면 해당 번호 상태 답변완료처리
+	adminQnaUpdate: `update qna set qna_status='답변완료' where qna_code =?`,
+	//관리자 답변 수정 .
+	adminUpdateReply: `update qna set ? where qna_code=?`,
+	adminAnswerinfo: `SELECT b.qna_code, b.title, b.write_date, b.content, a.qna_status, a.qna_divison, b.ans_code ,b.cnt FROM qna a JOIN qna b ON a.qna_code = b.ans_code WHERE b.ans_code = ?`,
+	//관리자 답변삭제
+	adminReplyDelete: `delete from qna where qna_code=?`,
+	//답변삭제하면 해당 질문글 답변대기로 업데이트
+	adminQnaWaitUpdate: `update qna set qna_status='답변대기' where qna_code =?`,
+
+	//판매자관리
+	adminSellerList: `select *from seller`, //판매자리스트
+	adminSellerInfo: `select s.seller_id, r.*from seller s left join restaurant r on (s.seller_id = r.seller_id) where s.seller_id = ?`, //판매자가 운영중인 업체정보
+	//공지사항 리스트
+	adminNoticeList: `select *from notice where user_division = ?`,
+	//공지사항 단건조회
+	adminNoticeInfo: `select *from notice where notice_code =?`,
+	//공지사항 등록
+	adminInsertNotice: `insert into notice set ?`,
+	//공지사항 등록 이미지 img 테이블에 인서트
+	noticeImgInsert: `insert into img set notice_code = ?, img_name = ?`,
+	//공지사항 이미지 가져오기
+	getNoticeImg: `select *from img where notice_code=?`,
+	//공지사항 수정
+	adminNoticeUpdate: `UPDATE notice SET ? WHERE notice_code = ?`,
+	//공지사항 삭제
+	adminNoticeDelete: `delete from notice where notice_code=?`,
+	//img 테이블 삭제
+	adminImgDelete: `delete from img where notice_code=?`,
+
+	//관리자---------------------------------------------------------------------------------------
 };
 
 // join : `insert into user(birthday, gender, grade, join_date, nickname, penalty, phone, profile, reserve_cnt, sns_status, user_id, user_name, user_pw, user_status, banned_cnt) values
